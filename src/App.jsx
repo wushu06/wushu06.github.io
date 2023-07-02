9be2e0b75ea65ebe42259d533412db5aa55b61fa
nour-latreche <nour.latreche@cigroup.co.uk>
2023-07-02 13:11:22 +0100

    gh pages

---
+++ src/App.jsx
@@ -0,0 +1,107 @@
+import React from 'react';
+
+function App() {
+	return (
+		<>
+
+			<header className="astro-ES6RJE63">
+				<nav
+					className="absolute z-10 w-full border-b border-black/5 dark:border-white/5 lg:border-transparent astro-ES6RJE63">
+					<div className="max-w-7xl mx-auto px-6 md:px-12 xl:px-6">
+						<div
+							className="relative flex flex-wrap items-center justify-between gap-6 py-3 md:gap-0 md:py-4 astro-ES6RJE63">
+							<div className="relative z-20 flex w-full justify-between md:px-0 lg:w-max astro-ES6RJE63">
+								<a href="/#home" aria-label="logo"
+								   className="flex items-center space-x-2 astro-ES6RJE63">
+									<div aria-hidden="true" className="flex space-x-1 astro-ES6RJE63">
+										<div
+											className="h-4 w-4 rounded-full bg-gray-900 dark:bg-white astro-ES6RJE63"></div>
+										<div className="h-6 w-2 bg-primary astro-ES6RJE63"></div>
+									</div>
+									<span
+										className="text-2xl font-bold text-gray-900 dark:text-white astro-ES6RJE63">NL</span>
+								</a>
+
+								<div className="relative flex max-h-10 items-center lg:hidden astro-ES6RJE63">
+									<button aria-label="humburger" id="hamburger"
+											className="relative -mr-6 p-6 astro-ES6RJE63">
+										<div aria-hidden="true" id="line"
+											 className="m-auto h-0.5 w-5 rounded bg-sky-900 transition duration-300 dark:bg-gray-300 astro-ES6RJE63"></div>
+										<div aria-hidden="true" id="line2"
+											 className="m-auto mt-2 h-0.5 w-5 rounded bg-sky-900 transition duration-300 dark:bg-gray-300 astro-ES6RJE63"></div>
+									</button>
+								</div>
+							</div>
+							<div id="navLayer" aria-hidden="true"
+								 className="fixed inset-0 z-10 h-screen w-screen origin-bottom scale-y-0 bg-white/70 backdrop-blur-2xl transition duration-500 dark:bg-gray-900/70 lg:hidden astro-ES6RJE63"></div>
+							<div id="navlinks"
+								 className="invisible absolute top-full left-0 z-20 w-full origin-top-right translate-y-1 scale-90 flex-col flex-wrap justify-end gap-6 rounded-3xl border border-gray-100 bg-white p-8 opacity-0 shadow-2xl shadow-gray-600/10 transition-all duration-300 dark:border-gray-700 dark:bg-gray-800 dark:shadow-none lg:visible lg:relative lg:flex lg:w-7/12 lg:translate-y-0 lg:scale-100 lg:flex-row lg:items-center lg:gap-0 lg:border-none lg:bg-transparent lg:p-0 lg:opacity-100 lg:shadow-none astro-ES6RJE63">
+								<div
+									className="w-full text-gray-600 dark:text-gray-200 lg:w-auto lg:pr-4 lg:pt-0 astro-ES6RJE63">
+									<ul className="flex flex-col gap-6 tracking-wide lg:flex-row lg:gap-0 lg:text-sm astro-ES6RJE63">
+										<li className="astro-ES6RJE63">
+											<a href="/#features"
+											   className="hover:text-primary block transition dark:hover:text-white md:px-4 astro-ES6RJE63">
+												<span className="astro-ES6RJE63">Blog</span>
+											</a>
+										</li>
+										<li className="astro-ES6RJE63">
+											<a href="/#solution"
+											   className="hover:text-primary block transition dark:hover:text-white md:px-4 astro-ES6RJE63">
+												<span className="astro-ES6RJE63">Work</span>
+											</a>
+										</li>
+									</ul>
+								</div>
+
+								<div className="mt-12 lg:mt-0 astro-ES6RJE63">
+									<a href="mailto:nour@latreche.co.uk"
+									   className="relative flex h-9 w-full items-center justify-center px-4 before:absolute before:inset-0 before:rounded-full before:bg-primary before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 sm:w-max astro-ES6RJE63">
+										<span className="relative text-sm font-semibold text-white astro-ES6RJE63"> Get in touch</span>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</nav>
+			</header>
+
+
+			<main className="space-y-40 mb-40">
+				<div className="relative" id="home">
+					<div aria-hidden="true"
+						 className="absolute inset-0 grid grid-cols-2 -space-x-52 opacity-40 dark:opacity-20">
+						<div
+							className="blur-[106px] h-56 bg-gradient-to-br from-primary to-purple-400 dark:from-blue-700"></div>
+						<div
+							className="blur-[106px] h-32 bg-gradient-to-r from-cyan-400 to-sky-300 dark:to-indigo-600"></div>
+					</div>
+					<div className="max-w-7xl mx-auto px-6 md:px-12 xl:px-6">
+						<div className="relative pt-36 ml-auto">
+							<div className="lg:w-2/3 text-center mx-auto">
+								<h1 className="text-gray-900 dark:text-white font-bold text-5xl md:text-6xl xl:text-7xl">
+									Noureddine <span className="text-primary dark:text-white"> Latreche.</span>
+								</h1>
+								<p className="mt-8 text-gray-700 dark:text-gray-300">
+									Full Stack Web Developer
+								</p>
+								<div className="mt-16 flex flex-wrap justify-center gap-y-4 gap-x-6">
+									<a href="mailto:nour@latreche.co.uk"
+									   className="relative flex h-11 w-full items-center justify-center px-6 before:absolute before:inset-0 before:rounded-full before:bg-primary before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 sm:w-max">
+										<span className="relative text-base font-semibold text-white">Get in touch</span>
+									</a>
+									<a href="#"
+									   className="relative flex h-11 w-full items-center justify-center px-6 before:absolute before:inset-0 before:rounded-full before:border before:border-transparent before:bg-primary/10 before:bg-gradient-to-b before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 dark:before:border-gray-700 dark:before:bg-gray-800 sm:w-max">
+										<span className="relative text-base font-semibold text-primary dark:text-white">Learn more</span>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</main>
+		</>
+	);
+}
+
+export default App;
